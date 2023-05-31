8327d9797b1bdf9cf4a84ec03dc4f537857e533f
allfree123 <daesungkim83@gmail.com>
2023-05-31 15:19:46 +0900

    no message

--- src/main/java/com/njonecompany/web/controller/WelcomeController.java
+++ src/main/java/com/njonecompany/web/controller/WelcomeController.java
@@ -16,8 +16,7 @@ public class WelcomeController {
     @GetMapping("/")
     public String index(Model model) {
         logger.debug("Welcome to njonecompany.com...");
-        //System.out.println(x: "index is called by GET /");
-
+       
         model.addAttribute("msg", getMessage());
         model.addAttribute("today", new Date());
         System.out.println(model.getAttribute("today"));
